9494b72b92a89a93dbd55d05ec0c6f8b1f2e8b54
takashabe <takashabe.public@gmail.com>
2013-02-17 13:45:43 +0900

    migration play 2.0.4 to play 2.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,17 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "konbu.md"
+    val appName         = "konbumd"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
       "com.tristanhunt" %% "knockoff" % "0.8.1"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here
     )
 
